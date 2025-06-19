1d7383681a6d6937b9a3c6b2fda9808a2244bf63
Max <139047507+maxxing9912@users.noreply.github.com>
2025-06-19 14:48:07 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
+import type { Configuration as WebpackConfiguration } from "webpack";
 
 const nextConfig: NextConfig = {
   env: {
     DISCORD_CLIENT_ID: "1370487453562962101",
     DISCORD_CLIENT_SECRET: "r-4-RqKenXJET83zteNpBkbVFqan9BCy",
-    NEXTAUTH_URL: "http://localhost:3000/",
+    NEXTAUTH_URL: "http://localhost:3000",
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       "pk_live_51RTquJGjdxMu1OKbBZaOIuS8IEV9EDYyAcaT2yCmdGXtzXpx4Vlhb0Vmye9E2EqoIa9j5zP5GWY0ahJXhKc5VAa400ZWwV02YU",
   },
@@ -12,11 +13,11 @@ const nextConfig: NextConfig = {
     domains: ["cdn.discordapp.com"],
   },
   webpack(
-    config: any,
+    config: WebpackConfiguration,
     { isServer }: { isServer: boolean }
-  ): any {
+  ): WebpackConfiguration {
     // Add node-loader
-    config.module.rules.push({
+    config.module!.rules!.push({
       test: /\.node$/,
       use: { loader: "node-loader" },
     });
@@ -26,7 +27,7 @@ const nextConfig: NextConfig = {
       config.externals = [
         ...(Array.isArray(config.externals)
           ? config.externals
-          : [config.externals]),
+          : [config.externals!]),
         {
           "discord.js": "commonjs2 discord.js",
           "@discordjs/ws": "commonjs2 @discordjs/ws",
