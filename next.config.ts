00b259d46267985c5fa76e36e7d01d7e6c7dfe80
Max <139047507+maxxing9912@users.noreply.github.com>
2025-06-19 14:50:40 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+// next.config.ts
 import type { NextConfig } from "next";
 import type { Configuration as WebpackConfiguration } from "webpack";
 
@@ -7,7 +8,7 @@ const nextConfig: NextConfig = {
     DISCORD_CLIENT_SECRET: "r-4-RqKenXJET83zteNpBkbVFqan9BCy",
     NEXTAUTH_URL: "http://localhost:3000",
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-      "pk_live_51RTquJGjdxMu1OKbBZaOIuS8IEV9EDYyAcaT2yCmdGXtzXpx4Vlhb0Vmye9E2EqoIa9j5zP5GWY0ahJXhKc5VAa400ZWwV02YU",
+      "pk_test_51RTquJGjdxMu1OKbOXLmErQAVDy7416CN8lraiP2YsPNH4ufZUFgNiuuQkNRNtnqyyc8hXG02rYYnNVJEm5uH1Re00pOIg6RO2",
   },
   images: {
     domains: ["cdn.discordapp.com"],
